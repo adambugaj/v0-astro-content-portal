778ce5239bd090a0a323f875dc72b462756d56a3
adambugaj <adampeter.bugaj@gmail.com>
2026-01-13 14:03:02 +0100

    13.01.2025 - v3

---
+++ src/data/koszt-budowy-domu-breadcrumbs.js
@@ -0,0 +1,5 @@
+export const breadcrumbs = [
+  { label: 'Strona główna', href: '/' },
+  { label: 'Budowa', href: '/budowa' },
+  { label: 'Koszt budowy domu', href: '/budowa/koszt-budowy-domu' }
+];
