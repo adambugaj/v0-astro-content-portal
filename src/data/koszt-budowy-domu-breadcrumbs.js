5e248e41df9a3766d3d97e54732bae8550d407f7
adambugaj <adampeter.bugaj@gmail.com>
2026-01-13 15:13:34 +0100

    13.01.2025 - v5

--- src/data/koszt-budowy-domu-breadcrumbs.js
+++ src/data/koszt-budowy-domu-breadcrumbs.js
@@ -4,3 +4,9 @@ export const breadcrumbs = [
   { label: 'Koszt budowy domu', href: '/budowa/koszt-budowy-domu' }
 ];
 export const scenariosTitle = 'Wybierz scenariusz budowy domu';
+export const pillarMeta = {
+  title: 'Koszt Budowy Domu 2026: Kompletny Przewodnik Inwestora',
+  description: 'Sprawdź aktualny koszt budowy domu w 2026 roku. Skorzystaj z kalkulatora budowlanego, checklisty i dowiedz się, jak zaplanować budżet.',
+  image: '/images/koszt-budowy-domu.webp',
+  href: '/budowa/koszt-budowy-domu'
+};
