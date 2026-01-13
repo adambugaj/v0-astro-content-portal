a104a8211000eaee077f0fc6caa10cdfb9332292
adambugaj <adampeter.bugaj@gmail.com>
2026-01-13 14:59:22 +0100

    13.01.2025 - v4

--- src/data/koszt-budowy-domu-breadcrumbs.js
+++ src/data/koszt-budowy-domu-breadcrumbs.js
@@ -3,3 +3,4 @@ export const breadcrumbs = [
   { label: 'Budowa', href: '/budowa' },
   { label: 'Koszt budowy domu', href: '/budowa/koszt-budowy-domu' }
 ];
+export const scenariosTitle = 'Wybierz scenariusz budowy domu';
